b96fe094a080cf08a91ad889efcda526d915a528
tjx12345 <18627888722@163.com>
2018-05-12 22:48:50 +0800

    23

--- sw.js
+++ sw.js
@@ -3,3 +3,16 @@ importScripts('sw-toolbox.js');
 toolbox.precache(["index.html"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
+
+ const _self = this;
+        _self.addEventListener('message',msg => alert(msg));
+        console.log('In service worker.');
+
+        _self.addEventListener('install', function () {
+            console.log('Install success');
+        });
+
+        _self.addEventListener('activate', function () {
+            console.log('Activated');
+        });
