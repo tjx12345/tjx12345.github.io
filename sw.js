5a983bdee5e956d2e7d6a9cf780770817abca3d7
tjx12345 <18627888722@163.com>
2018-05-12 20:37:47 +0800

    14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-importScripts('sw-toolbox.js');
+importScripts('sw-sstoolbox.js');
 toolbox.precache(["index.html", "style/style.css"]);
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
